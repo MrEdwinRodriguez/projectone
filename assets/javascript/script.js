6f8678b9c3571e781184247a76b14f318c3c8bec
Edwin Rodriguez <Edwin Rodriguez>
2016-08-09 20:29:48 -0400

    upgrade

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,40 @@
 // Karaoke Roulette JavaScript
+
+// $(document).on('click', 'button', function(){
+//$('button').on('click', function(){
+
+// var searchWord = 'karaoke';
+// // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+// var queryURL = "https://userstream.twitter.com/1.1/user.json";
+
+
+// $.ajax({
+//   url: queryURL,
+//   method: 'GET'
+
+// })
+
+
+// .done(function(response){
+
+//   console.log(response);
+
+//   //var results = response.data;
+
+
+
+// });
+
+$(document).ready(function(){
+
+	$('#btn1').on('click', function(){
+
+
+		console.log('hello');
+		// Youtube video call goes here
+
+
+	});
+
+});
