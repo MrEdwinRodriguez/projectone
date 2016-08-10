b87b0504527391f6e993acbec5dd93dc04c54130
creativelaurels <lauren@creativelaurels.com>
2016-08-09 21:02:08 -0400

    Completed firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,20 +10,35 @@ $("#gameButton").on("click", function() {
 
 
 /// THIS PART RESETS THE FIREBASE COUNTERS ///
-	// Set the clickCounter back to initialValue
+	// Set the emoji counters back to zero
 	thumbsupCounter = 0;
-  thumbsupCount=0
+  thumbsupCount = 0;
+  thumbsdownCount = 0;
+  thumbsdownCounter = 0;
+  chickenCount = 0;
+  chickenCounter = 0;
+  flatCount = 0;
+  flatCounter = 0;
 
 	// Save new value to Firebase
 	database.ref().set({
 		thumbsupCount: thumbsupCounter
+    // thumbsdownCount: thumbsdownCounter
+    // chickenCount: chickenCounter
+    // flatCount: flatCounter
 	});
 
 	// Log the value of clickCounter
 	console.log(thumbsupCounter);
+  console.log(thumbsdownCounter);
+  console.log(chickenCounter);
+  console.log(flatCounter);
 
 	// Change the HTML Values
 	$('#thumbsupValue').html(thumbsupCounter);
+  $('#thumbsdownValue').html(thumbsdownCounter);
+  $('#chickenValue').html(chickenCounter);
+  $('#flatValue').html(flatCounter);
 
 
 });
